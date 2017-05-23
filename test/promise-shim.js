18e59f8e4b6c5c6b5a8edb1ef5104c17b3d6d401
apla <dot.and.thing@gmail.com>
2017-05-23 16:28:56 +0300

    fixing for the old nodejs

--- test/promise-shim.js
+++ test/promise-shim.js
@@ -1,3 +1,6 @@
-if (typeof Promise === 'undefined') {
+var nodeVer = process.version.substr (1).split ('.');
+
+if (nodeVer[0] < 4) {
 	global.Promise = require ('bluebird');
+	Object.assign  = require ('object-assign');
 }
